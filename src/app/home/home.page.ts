0ffc0581f839216ce397a7968f3bae9b82cc3d52
Oliver Brown <akuma_shadow@hotmail.com>
2019-12-16 19:06:54 -0400

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { Howl } from 'howler';
+import { IonRange } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,88 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  playlist: Track[] = [
+    {
+      name: 'Dj SantiagoCejas-Avengers',
+      path: './assets/mp3/Avengers-DjSantiagoCejas.mp3'
+    },
+    {
+      name: 'Dj SantiagoCejas-Avengers2',
+      path: './assets/mp3/Avengers-DjSantiagoCejas.mp3'
+    },
+    {
+      name: 'Dj SantiagoCejas-Avengers3',
+      path: './assets/mp3/Avengers-DjSantiagoCejas.mp3'
+    }
+  ];
+
+  activeTrack: Track = null;
+  player: Howl = null;
+  isPlaying = false;
+  progress = 0;
+  @ViewChild('range', {static: false}) range: IonRange;
+
   constructor() {}
+
+  start(track: Track) {
+    if (this.player) {
+      this.player.stop();
+    }
+    this.player = new Howl({
+      src: [track.path],
+      html5: true,
+      onplay: () => {
+        console.log('onplay');
+        this.isPlaying = true;
+        this.activeTrack = track;
+        this.updateProgress();
+      },
+      onend: () => {
+      console.log('onend');
+      }
+    });
+    this.player.play();
+  }
+
+  togglePlayer(pause) {
+    this.isPlaying = !pause;
+    if (pause) {
+      this.player.pause();
+    } else {
+      this.player.play();
+    }
+  }
+
+  next() {
+    let index = this.playlist.indexOf(this.activeTrack);
+    if (index != this.playlist.length - 1) {
+      this.start(this.playlist[index + 1]);
+    } else {
+      this.start(this.playlist[0]);
+    }
+  }
+
+  prev() {
+    let index = this.playlist.indexOf(this.activeTrack);
+    if (index > 0) {
+      this.start(this.playlist[index - 1]);
+    } else {
+      this.start(this.playlist[this.playlist.length - 1]);
+    }
+  }
+
+  seek() {
+    let newValue = +this.range.value;
+    let duration = this.player.duration();
+    this.player.seek(duration * (newValue / 100));
+  }
+
+  updateProgress() {
+    let seek = this.player.seek();
+    this.progress = (seek / this.player.duration()) * 100 || 0;
+    setTimeout(() => {
+      this.updateProgress();
+    }, 1000)
+  }
 
 }
